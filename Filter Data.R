fa885458969e50af6310b66993663242db0423ab
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-11-13 13:51:38 -0600

    Filter Data.R

---
+++ Filter Data.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library("dplyr")
+
+bill <- read.csv("500_Cities__Local_Data_for_Better_Health.csv")
+
+print(bill)
+
+State_description <- bill %>%
+  select(Year, StateAbbr, StateDesc, CityName,PopulationCount, Category, Measure, Short_Question_Text, Data_Value_Type, Data_Value, Low_Confidence_Limit,High_Confidence_Limit)%>%
+  filter(StateAbbr =="IL")%>%
+  filter(Category =="Health Outcomes")%>%
+  arrange(desc(PopulationCount))
