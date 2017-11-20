9dbc49b2a6f44713d8abdb92faa608f99afb5ce8
sdduffield <samuel.duffield@stlcop.edu>
2017-11-20 14:58:48 -0600

    graphic data

---
+++ 11-20.R
@@ -0,0 +1,25 @@
+setwd("~/Intro to Data Science/Project/data")
+
+library(tidyverse)
+library("dplyr")
+
+bill <- read.csv("500_Cities__Local_Data_for_Better_Health.csv")
+
+print(bill)
+
+State_description <- bill %>%
+  select(Year, StateAbbr, Category, CityName, Measure, PopulationCount, Data_Value, Low_Confidence_Limit,High_Confidence_Limit)%>%
+  filter(StateAbbr =="IL")%>%
+  filter(Category =="Health Outcomes")%>%
+  filter(Year == 2014)%>%
+  mutate(PopulationCount, PopIncidence = Data_Value * PopulationCount * 0.01)%>%
+  group_by(CityName, Measure, Year)%>%
+  summarise(IncidenceRate=sum(PopIncidence, na.rm = TRUE), Incidence = IncidenceRate/sum(PopulationCount))
+  
+ 
+
+ 
+  
+  
+View(State_description)
+View(bill)
