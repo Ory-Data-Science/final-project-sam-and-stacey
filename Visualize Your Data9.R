7a6aa881ca1cc2619ffc566365348f88d71a0a48
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-11-27 13:39:09 -0600

    Chronic obstructive pulmonary disease among adults aged >=18 Years

---
+++ Visualize Your Data9.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library("dplyr")
+
+bill <- read.csv("500_Cities__Local_Data_for_Better_Health.csv")
+
+print(bill)
+
+State_description <- bill %>%
+  select(Year, StateAbbr, Category, CityName, Measure, PopulationCount, Data_Value, Low_Confidence_Limit,High_Confidence_Limit)%>%
+  filter(StateAbbr =="IL")%>%
+  filter(Category =="Health Outcomes")%>%
+  filter(Measure =="Chronic obstructive pulmonary disease among adults aged >=18 Years")%>%
+  filter(Year == 2014)%>%
+  mutate(PopulationCount, PopIncidence = Data_Value * PopulationCount * 0.01)%>%
+  group_by(CityName, Measure, Year)%>%
+  summarise(IncidenceRate=sum(PopIncidence, na.rm = TRUE), Incidence = IncidenceRate/sum(PopulationCount))
+
+
+
+
+
+
+View(State_description)
+View(bill)
+s <- ggplot(State_description,aes(CityName,Incidence))
+s + ggtitle("Chronic obstructive pulmonary disease among adults aged >=18 Years")+ geom_col()
