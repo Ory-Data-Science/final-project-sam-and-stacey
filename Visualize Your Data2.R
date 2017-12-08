da69d8fa96886f5b1ea848a7f4917b0eb201570b
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-12-08 09:24:24 -0600

    Arthritis among adults aged >=18 Years graph

--- Visualize Your Data2.R
+++ Visualize Your Data2.R
@@ -22,5 +22,5 @@ State_description <- bill %>%
 
 View(State_description)
 View(bill)
-s <- ggplot(State_description,aes(CityName,Incidence,Incidence,fill=CityName))
+s <- ggplot(State_description,aes(x=CityName,y=Incidence,fill=CityName))
 s + ggtitle("Arthritis among adults aged >=18 Years")+  coord_flip() + geom_col()+ scale_fill_manual(values=c("aquamarine2", "aquamarine2","pink3","aquamarine2","pink3","aquamarine2","aquamarine2","pink3","aquamarine2","aquamarine2","aquamarine2","aquamarine2","aquamarine2","pink3","pink3","aquamarine2","pink3","aquamarine2"))
