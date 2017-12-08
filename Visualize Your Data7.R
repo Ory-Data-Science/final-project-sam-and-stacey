32a661da22fa4108c2009824854f5c63b271808c
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-12-08 06:51:48 -0600

    Current asthma among adults aged >=18 Years

--- Visualize Your Data7.R
+++ Visualize Your Data7.R
@@ -23,4 +23,4 @@ State_description <- bill %>%
 View(State_description)
 View(bill)
 s <- ggplot(State_description,aes(x=CityName,y=Incidence,fill=CityName))
-s + ggtitle("Current asthma among adults aged >=18 Years")+ coord_flip()+ geom_col() + scale_fill_manual(values=c("green", "green","yellow","green","yellow","green","green","yellow","green","green","green","green","green","yellow","yellow","green","yellow","green"))
+s + ggtitle("Current asthma among adults aged >=18 Years")+ coord_flip()+ geom_col() + scale_fill_manual(values=c("green", "green","purple","green","purple","green","green","purple","green","green","green","green","green","purple","purple","green","purple","green"))
