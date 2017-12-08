7199e313b79c704c4652a8a1eff956f604aa8505
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-12-08 10:06:06 -0600

    Diagnosed diabetes among adults aged >=18 Years graph

--- Visualize Your Data8.R
+++ Visualize Your Data8.R
@@ -23,4 +23,4 @@ State_description <- bill %>%
 View(State_description)
 View(bill)
 s <- ggplot(State_description,aes(x=CityName,y=Incidence,fill=CityName))
-s + ggtitle("Diagnosed diabetes among adults aged >=18 Years")+  coord_flip() + geom_col()+ scale_fill_manual(values=c("blue4", "blue4","orange2","blue4","orange2","blue4","blue4","orange2","blue4","blue4","blue4","blue4","blue4","orange2","orange2","blue4","orange2","blue4"))
+s + ggtitle("Diagnosed diabetes among adults aged >=18 Years")+  coord_flip() + geom_col() + scale_fill_manual(values=c("blue4", "blue4","orange2","blue4","orange2","blue4","blue4","orange2","blue4","blue4","blue4","blue4","blue4","orange2","orange2","blue4","orange2","blue4"))
