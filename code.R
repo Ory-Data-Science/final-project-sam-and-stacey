f0db2f4b138a2abd243ab464f7052da68e953d1e
sdduffield <samuel.duffield@stlcop.edu>
2017-12-08 19:16:53 -0600

    cleaning unnecessary code

--- code.R
+++
@@ -1,5 +0,0 @@
-State_description <- bill %>%
-  select(StateAbbr, Category, CityName, Measure, PopulationCount, Data_Value, Low_Confidence_Limit,High_Confidence_Limit)%>%
-  filter(StateAbbr =="IL")%>%
-  filter(Category =="Health Outcomes")%>%
-  group_by(CityName, Measure)
